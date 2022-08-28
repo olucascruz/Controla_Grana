6f25c9e780f278169c57c819c09a022e3eacb219
LucasCruz <lca.snf21@uea.edu.br>
2022-08-28 15:23:27 -0400

    add list and button

--- app/src/main/java/com/example/controleagrana/activities/MainActivity.java
+++ app/src/main/java/com/example/controleagrana/activities/MainActivity.java
@@ -3,14 +3,44 @@ package com.example.controleagrana.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.controleagrana.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listUsers;
+
+    private String[] users = {
+        "oLucas", "aLucas", "eLucas"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listUsers = findViewById(R.id.listUsers);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_activated_1,
+                android.R.id.text1, users
+
+        );
+
+        listUsers.setAdapter(adapter);
+
+        listUsers.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                String nomeSelecionado = listUsers.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), nomeSelecionado, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
